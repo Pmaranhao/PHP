2dfafc10801fe6df47d1f8d070372e28ba06371f
Pedro Alves <pedro.maranhao@outlook.com.br>
2020-05-20 18:13:53 -0300

    atualizando

--- php_oo/oo_abstracao.php
+++ php_oo/oo_abstracao.php
@@ -192,6 +192,37 @@ class Funcionario4 {
 	$f-> __set('cargo', 'Supervisora');
 	$f-> __set('salario', 'R$ 5.000,00');
 	echo $f->resumirCadFunc();
+?>
 
+<h1>Método construtor e Destrutor</h1>
+
+
+<?php
+	class Pessoa {
+		public $nome = null;
+
+		function __construct($nome) {
+			echo 'Objeto iniciado';
+			$this->nome = $nome;
+		}
+
+		function __destruct() {
+			echo 'Objeto removido';
+		}
+
+		function __get($atributo) {
+			return $this->$atributo;
+		}
+
+		function correr() {
+			return $this->__get('nome') . ' está correndo';
+		}
+	}
+
+	$pessoa = new Pessoa('Jorge');
+	echo '<br/> Nome: ' . $pessoa->__get('nome');
+	echo '<br/> ' . $pessoa->correr();
+	echo '<br/>';
+	unset($pessoa);
 
 ?>
